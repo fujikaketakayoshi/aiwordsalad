ff9c9be9905f9b49b818c6621b939acd81a83fbd
fjkk <fujikake.takayoshi@gmail.com>
2020-10-04 17:56:25 +0900

    html.class added

---
+++ phplib/html.class.php
@@ -0,0 +1,36 @@
+<?php
+namespace Html;
+
+function header ($title, $root_url) {
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?= $title ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header>
+  <nav class="my-navbar">
+    <a class="my-navbar-brand" href="<?= $root_url ?>"><?= $title ?></a>
+  </nav>
+</header>
+<main>
+<?php
+}
+
+function footer () {
+?>
+</main>
+<!-- scripts -->
+</body>
+</html>
+<?php
+}
+
+
--- search.php
+++ search.php
@@ -1,102 +1,147 @@
 <?php
-require('phplib/keyvalue_file.class.php');
+require_once('phplib/keyvalue_file.class.php');
 require_once('phplib/crawler.class.php');
+require_once('phplib/html.class.php');
 
 use KeyValueFile\KeyValueFile;
 use Crawler\HB;
 
 $keyword = isset($_SERVER['PATH_INFO']) ? str_replace("/", "", $_SERVER['PATH_INFO']) : '';
+$url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
+$index_url = str_replace("search.php", "index.php", $url);
+
 if ( $keyword == '') {
-	$url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
-	$index_url = str_replace("search.php", "index.php", $url);
 	header("Location: $index_url");
 	exit;
 }
 
-$tou_array = [];
-$ku_array = [];
+$wordsalads = [];
+$file = new KeyValueFile('phplib/tmp');
 
-$file = new KeyValueFile('phplib/tmp', ['expires' => true]);
-$key = 'tou_ku_array';
-if ( $file->is_cache_available($key) ) {
-	$arr = $file->get_keyvalue($key);
-	$tou_array = $arr['tou'];
-	$ku_array = $arr['ku'];
+// ワードサラダが保存されていれば取り出し、無ければ作成
+if ( $file->has_key($keyword) ) {
+	$wordsalads = $file->get_keyvalue($keyword);
 } else {
-	$hb = new HB();
-	
-	// 不適切記号を句読点化
-	$str = str_replace("...", "、", $hb->get_desc_str());
-	$str = str_replace("、。", "。", $str);
-	$str = str_replace("。、", "。", $str);
-	$str = str_replace("？", "。", $str);
-	$str = str_replace("！", "。", $str);
-	$str = str_replace("■", "。", $str);
-	$str = str_replace("➡︎", "。", $str);
-	$str = str_replace(" ", "。", $str);
-	$str = str_replace("＝", "。", $str);
-	$str = str_replace("「", "", $str);
-	$str = str_replace("」", "", $str);
-	
-	
-	preg_match_all("/([^、。].*?)、/u", $str, $tou_match_arr);
-	
 	$tou_array = [];
 	$ku_array = [];
 	
-	foreach ( $tou_match_arr[1] as $m ) {
-		$arr = explode("。", $m);
-		if ( count($arr) == 1 ) {
-			$tou_array[] = $arr[0];
-		} else {
-			$tmparr = array_slice($arr, 0, count($arr) - 1);
-			$ku_array[] = $tmparr[0];
-		}
-	}
-	
 	$file = new KeyValueFile('phplib/tmp', ['expires' => true]);
-	$file->set_expire_span(60*60);
-	$arr = [];
-	$arr['tou'] = $tou_array;
-	$arr['ku'] = $ku_array;
-	$file->set_keyvalue('tou_ku_array', $arr);
-	$arr = $file->get_keyvalue($key);
-	$tou_array = $arr['tou'];
-	$ku_array = $arr['ku'];
-}
-
-shuffle($tou_array);
-shuffle($ku_array);
-
-$wordsalads = [];
-
-// 7個のワードサラダブロックを作る
-foreach ( range(1, 7) as $i ) {
-	$line_words = [];
-	
-	// 9句読点を配列に入れて最後は句点を入れる、20%の確率で各文にキーワードを挿入
-	foreach ( range(1, 9) as $ii ) {
-		if ( rand(0, 1) === 0 ) {
-			$tmp = array_shift($tou_array);
-			$tmp = rand(0, 5) === 0 ? $keyword . $tmp : $tmp;
-			$line_words[] = $tmp . "、";
-		} else {
-			$tmp = array_shift($ku_array);
-			$tmp = rand(0, 5) === 0 ? $keyword . $tmp : $tmp;
-			$line_words[] =  $tmp . "。";
+	$key = 'tou_ku_array';
+	if ( $file->is_cache_available($key) ) {
+		$arr = $file->get_keyvalue($key);
+		$tou_array = $arr['tou'];
+		$ku_array = $arr['ku'];
+	} else {
+		$hb = new HB();
+		
+		// 不適切記号を句読点化
+		$str = str_replace("...", "、", $hb->get_desc_str());
+		$str = str_replace("、。", "。", $str);
+		$str = str_replace("。、", "。", $str);
+		$str = str_replace("？", "。", $str);
+		$str = str_replace("！", "。", $str);
+		$str = str_replace("•", "。", $str);
+		$str = str_replace("◦", "。", $str);
+		$str = str_replace("■", "。", $str);
+		$str = str_replace("➡︎", "。", $str);
+		$str = str_replace("（", "、", $str);
+		$str = str_replace("）", "。", $str);
+		$str = str_replace(" ", "", $str);
+		$str = str_replace(" ", "。", $str);
+		$str = str_replace("＝", "。", $str);
+		$str = str_replace("「", "", $str);
+		$str = str_replace("」", "", $str);
+		
+		
+		preg_match_all("/([^、。].*?)、/u", $str, $tou_match_arr);
+		
+		$tou_array = [];
+		$ku_array = [];
+		
+		foreach ( $tou_match_arr[1] as $m ) {
+			$arr = explode("。", $m);
+			if ( count($arr) == 1 ) {
+				$tou_array[] = $arr[0];
+			} else {
+				$tmparr = array_slice($arr, 0, count($arr) - 1);
+				$ku_array[] = $tmparr[0];
+			}
 		}
+		
+		$file = new KeyValueFile('phplib/tmp', ['expires' => true]);
+		$file->set_expire_span(60*60);
+		$arr = [];
+		$arr['tou'] = $tou_array;
+		$arr['ku'] = $ku_array;
+		$file->set_keyvalue('tou_ku_array', $arr);
+		$arr = $file->get_keyvalue($key);
+		$tou_array = $arr['tou'];
+		$ku_array = $arr['ku'];
 	}
 	
-	$line_words[] = array_shift($ku_array) . "。";
-	$wordsalads[] = implode("", $line_words);
+	shuffle($tou_array);
+	shuffle($ku_array);
 	
-	if ( count($ku_array) === 0 ) break;
+	// 7個のワードサラダブロックを作る
+	foreach ( range(1, 7) as $i ) {
+		$line_words = [];
+		
+		// 9句読点を配列に入れて最後は句点を入れる、20%の確率で各文にキーワードを挿入
+		foreach ( range(1, 9) as $ii ) {
+			if ( rand(0, 1) === 0 ) {
+				$tmp = array_shift($tou_array);
+				$tmp = rand(0, 5) === 0 ? $keyword . $tmp : $tmp;
+				$line_words[] = $tmp . "、";
+			} else {
+				$tmp = array_shift($ku_array);
+				$tmp = rand(0, 5) === 0 ? $keyword . $tmp : $tmp;
+				$line_words[] =  $tmp . "。";
+			}
+		}
+		
+		$line_words[] = array_shift($ku_array) . "。";
+		$wordsalads[] = implode("", $line_words);
+		
+		if ( count($ku_array) === 0 ) break;
+	}
+	
+	var_dump($tou_array);
+	echo "<br>";
+	var_dump($ku_array);
+	
+	// ワードサラダを保存
+	$file = new KeyValueFile('phplib/tmp');
+	$file->set_keyvalue($keyword, $wordsalads);
 }
 
+//echo "<br>";
+//var_dump($wordsalads);
 
+Html\header('AI Wordsalad', $index_url);
+?>
+	<div class="container">
+		<div class="row">
+			<div class="col col-md-4">
+				<nav class="panel panel-default">
+					<div class="panel-heading"><?= $keyword ?>のAIワードサラダ</div>
+<!--
+					<div class="panel-body">
+						<a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
+							フォルダを追加する
+						</a>
+					</div>
+-->
+					<div class="list-group">
+					<?php foreach ($wordsalads as $ws) { ?>
+						<div class="list-group-item">
+								<?= $ws ?>
+						</div>
+					<?php } ?>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+<?php
+Html\footer();
 
-var_dump($wordsalads);
-echo "<br>";
-var_dump($tou_array);
-echo "<br>";
-var_dump($ku_array);
