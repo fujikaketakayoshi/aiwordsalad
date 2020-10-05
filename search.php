51bbe1d66b8f8a47f5979c126cc5b48caa8cad8f
fjkk <fujikake.takayoshi@gmail.com>
2020-10-06 07:32:33 +0900

    css fixed responsive2

--- search.php
+++ search.php
@@ -123,7 +123,7 @@ Html\header($keyword . 'のAI Wordsalad', $index_url);
 ?>
 	<div class="container">
 		<div class="row">
-			<div class="col col-md-offset-2 col-md-8">
+			<div class="col col-md-offset-1 col-md-10">
 <!--
 			<div class="col col-md-4">
 			-->
