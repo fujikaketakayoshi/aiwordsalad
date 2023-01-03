7d45476b01fdf092755d7a75b717c13d78ef4ace
fjkk <fujikake.takayoshi@gmail.com>
2023-01-03 17:31:44 +0900

    https debug

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once('phplib/html.class.php');
 use KeyValueFile\KeyValueFile;
 
 
+var_dump($_SERVER);
+
 $protocol = isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) ? $_SERVER["HTTP_X_FORWARDED_PROTO"] : 'http';
 $url = $protocol . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
 $index_url = str_replace("index.php", "", $url);
