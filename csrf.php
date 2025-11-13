801bea5e7614092e8ddd5c110bcef10ebedc834a
fjkk <fujikake.takayoshi@gmail.com>
2025-11-13 20:51:22 +0900

    csrf delete

--- csrf.php
+++
@@ -1,5 +0,0 @@
-<form action="http://localhost/twitterclone_dev/Controllers/post.php" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="token" value="45510e85addc5513a10bdb7944cc122115677d954b46fe28804f7dff010c8862">
-	<input type="hidden" name="body" value="こんにちはこんにちは">
-    <button class="btn" type="submit">進む</button>
-</form>
--- csrf2.php
+++
@@ -1,7 +0,0 @@
-<form action="http://127.0.0.1:8000/task" method="POST" class="form-horizontal">
-    <input type="hidden" name="_token" value="wu21lvKUXN2Gy5rn5dMdfIqZdWbKOuvRX6ow2ILo">
-    <input type="hidden" name="name" value="こんばんはこんばんは" id="task-name" class="form-control">
-    <button type="submit" class="btn btn-default">
-        <i class="fa fa-plus">Add Task</i>
-    </button>
-</form>
