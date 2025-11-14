eaabd943c0f31e0a6696f6afb40635646f8187c9
fjkk <fujikake.takayoshi@gmail.com>
2025-11-14 20:29:22 +0900

    csrftest

--- csrf.php
+++ csrf.php
@@ -1,6 +1,4 @@
 <form action="http://localhost/twitterclone_dev/Controllers/post.php" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="token" value="bec1160dc55217e5e929d42bfe95b49794dc98344083839344e2025aeb421162">
-
     <input type="hidden" name="body" value="こんにちはこんにちは">
     <button class="btn" type="submit">進む</button>
 </form>
