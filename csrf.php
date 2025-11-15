27277754bc818653fd174239e34deeccb733cdc8
fjkk <fujikake.takayoshi@gmail.com>
2025-11-15 17:16:33 +0900

    csrf added

---
+++ csrf.php
@@ -0,0 +1,4 @@
+ <form action="http://localhost/twitterclone_dev/Controllers/post.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="body" value="こんにちはこんにちは">
+    <button class="btn" type="submit">進む</button>
+</form>
