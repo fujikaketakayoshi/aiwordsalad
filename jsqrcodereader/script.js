9dc89745800d4e0417268458fae46a6e2e81f37f
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-02-27 11:24:58 +0900

    スマホの背面カメラ指定

--- jsqrcodereader/script.js
+++ jsqrcodereader/script.js
@@ -3,7 +3,7 @@ const video = document.getElementById('video');
 let contentWidth;
 let contentHeight;
 
-const media = navigator.mediaDevices.getUserMedia({ audio: false, video: {width:640, height:480} })
+const media = navigator.mediaDevices.getUserMedia({ audio: false, video: {width:640, height:480, facingMode: "environment"} })
    .then((stream) => {
       video.srcObject = stream;
       video.onloadeddata = () => {
